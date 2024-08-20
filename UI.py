291485d57b7fc73f653e9a8bee853aa721674e9c
Keval Solanki <solankikeval166@gmail.com>
2024-08-20 12:19:43 +0530

    Prompt update

--- UI.py
+++ UI.py
@@ -79,15 +79,17 @@ def initialize_vectorstore(text):
 
 
 def initialize_qa_chain(llm, vectorstore):
-    prompt_template = """Use the following pieces of context to answer the question at the end. Please follow the following rules:
-    1. If you don't know the answer, don't try to make up an answer. Just say "I can't find the final answer but you may want to check the following links".
+    prompt_template = """
+    Use the following pieces of context to answer the question at the end. Please follow the following rules:
+    1. If you don't know the answer, don't try to make up an answer. 
+    Just say "I can't find the final answer but you may want to check the following links".
     2. If you find the answer, write the answer in a concise way with five sentences maximum.
 
     {context}
 
     Question: {question}
 
-    Helpful Answer:
+    Answer:
     """
     PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
